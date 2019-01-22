52a502af7af23ad887acd53f196367a334e682ff
Stephan Toggweiler <stephan.toggweiler@hispeed.ch>
2019-01-22 11:17:21 +0100

    Update kcgi.rb

--- kcgi.rb
+++ kcgi.rb
@@ -2,7 +2,7 @@ class Kcgi < Formula
 	desc "minimal CGI and FastCGI library in C"
 	homepage "https://kristaps.bsd.lv/kcgi"
 	url "https://kristaps.bsd.lv/kcgi/snapshots/kcgi-0.10.10.tgz"
-	sha256 "7bb64212262de06c1203a326cf5873aebac3b80fa35ee9dc7e13a494a762723f"
+	sha256 "ca254027809c3f4e730ec21fec92d76d709c2f2827aa7d1becc59572ead427a3"
 	def install
 		system "./configure", "CPPFLAGS=-I#{include}", "LDFLAGS=-L#{lib}", "PREFIX=#{prefix}", "LIBDIR=#{lib}", "MANDIR=#{man}", "INCLUDEDIR=#{include}", "SHAREDIR=#{share}", "SBINDIR=#{sbin}"
 		system "make", "install"
