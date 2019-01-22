0f01323d6c7153797260e28cdc9960bb21daf687
Stephan Toggweiler <stephan.toggweiler@hispeed.ch>
2019-01-22 13:07:19 +0100

    Update kwebapp.rb

--- kwebapp.rb
+++ kwebapp.rb
@@ -1,8 +1,8 @@
 class Kwebapp < Formula
 	desc "web application source generator"
 	homepage "https://kristaps.bsd.lv/kwebapp"
-	url "https://kristaps.bsd.lv/kwebapp/snapshots/kwebapp-0.6.5.tar.gz"
-	sha256 "e5c596af8109520d866f47a2f4033965d46aa7e625051482d3c4190b8a0fc8d2"
+	url "https://kristaps.bsd.lv/kwebapp/snapshots/kwebapp-0.6.9.tar.gz"
+	sha256 "b2bf1e0a3f7609e53e0877eb033c9a8a5e847628433aeb3d2e1e0f202bcad18e"
 	def install
 		system "./configure", "CPPFLAGS=-I#{include}", "LDFLAGS=-L#{lib}", "PREFIX=#{prefix}", "MANDIR=#{man}", "SHAREDIR=#{share}"
 		system "make", "install"
