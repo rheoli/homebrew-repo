86697d9386f00ef5754ec5d60c1284fb929fd96c
Stephan Toggweiler <stephan.toggweiler@hispeed.ch>
2019-01-22 13:09:09 +0100

    Update lowdown.rb

--- lowdown.rb
+++ lowdown.rb
@@ -1,8 +1,8 @@
 class Lowdown < Formula
 	desc "simple Markdown translator"
 	homepage "https://kristaps.bsd.lv/lowdown"
-	url "https://kristaps.bsd.lv/lowdown/snapshots/lowdown-0.4.1.tar.gz"
-	sha256 "a5fb2fa51da9c2dcbd22332e71a8b39fc838b90299e0ac8cdfffd2bc7934fcec"
+	url "https://kristaps.bsd.lv/lowdown/snapshots/lowdown-0.4.2.tar.gz"
+	sha256 "2a9d318a4f0898dd6cb65a46ad5cdf23d85660fcbbcebe19aa4828a4ca2f5c04"
 	depends_on "kristapsdz/repo/libdiff" => :build
 	def install
 		system "./configure", "PREFIX=#{prefix}", "MANDIR=#{man}", "SHAREDIR=#{share}", "BINDIR=#{bin}", "INCLUDEDIR=#{include}", "LIBDIR=#{lib}"
