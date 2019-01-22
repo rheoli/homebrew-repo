f0a1aa4f34f6c3aa7880e412eec269f6edbeb840
Stephan Toggweiler <stephan.toggweiler@hispeed.ch>
2019-01-22 13:05:27 +0100

    Update kcgi.rb

--- kcgi.rb
+++ kcgi.rb
@@ -3,6 +3,7 @@ class Kcgi < Formula
 	homepage "https://kristaps.bsd.lv/kcgi"
 	url "https://kristaps.bsd.lv/kcgi/snapshots/kcgi-0.10.10.tgz"
 	sha256 "ca254027809c3f4e730ec21fec92d76d709c2f2827aa7d1becc59572ead427a3"
+	depends_on "bmake" => :build
 	def install
 		system "./configure", "CPPFLAGS=-I#{include}", "LDFLAGS=-L#{lib}", "PREFIX=#{prefix}", "LIBDIR=#{lib}", "MANDIR=#{man}", "INCLUDEDIR=#{include}", "SHAREDIR=#{share}", "SBINDIR=#{sbin}"
 		system "bmake", "install"
